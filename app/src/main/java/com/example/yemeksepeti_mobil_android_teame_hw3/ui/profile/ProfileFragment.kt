1ca148bd3329b96bec5040de0ec9bc461b645e65
Cüneyt Atahan Eyüboğlu <ca.eyuboglu@gmail.com>
2021-08-11 19:24:39 +0300

    Logout Module

--- app/src/main/java/com/example/yemeksepeti_mobil_android_teame_hw3/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/yemeksepeti_mobil_android_teame_hw3/ui/profile/ProfileFragment.kt
@@ -1,18 +1,25 @@
 package com.example.yemeksepeti_mobil_android_teame_hw3.ui.profile
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import com.example.yemeksepeti_mobil_android_teame_hw3.activity.StartActivity
 import com.example.yemeksepeti_mobil_android_teame_hw3.data.entity.hotels.hotel.User
 import com.example.yemeksepeti_mobil_android_teame_hw3.data.entity.user.UserData
 import com.example.yemeksepeti_mobil_android_teame_hw3.data.entity.user.UserResponse
+import com.example.yemeksepeti_mobil_android_teame_hw3.data.local.SharedPrefManager
 import com.example.yemeksepeti_mobil_android_teame_hw3.databinding.FragmentProfileBinding
 import com.example.yemeksepeti_mobil_android_teame_hw3.util.Resource
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.qualifiers.ActivityContext
+import dagger.hilt.android.qualifiers.ApplicationContext
 
 @AndroidEntryPoint
 class ProfileFragment: Fragment() {
@@ -44,5 +51,16 @@ class ProfileFragment: Fragment() {
                 }
             }
         })
+        _binding.btnSignOut.setOnClickListener{
+            if(viewModel.signOut()){
+                activity?.let{
+                    val intent = Intent(it, StartActivity::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+                    startActivity(intent)
+                    it.finish()
+                }
+            }
+
+        }
     }
 }
