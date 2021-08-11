c0dddd2a95858a40afe52f99bf9dbfe01df3c6f9
Cüneyt Atahan Eyüboğlu <ca.eyuboglu@gmail.com>
2021-08-11 12:45:41 +0300

    Get profile with Token

--- app/src/main/java/com/example/yemeksepeti_mobil_android_teame_hw3/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/yemeksepeti_mobil_android_teame_hw3/ui/profile/ProfileViewModel.kt
@@ -1,10 +1,8 @@
 package com.example.yemeksepeti_mobil_android_teame_hw3.ui.profile
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.yemeksepeti_mobil_android_teame_hw3.data.TravelApiRepository
-import com.example.yemeksepeti_mobil_android_teame_hw3.data.entity.user.UserRequest
 import com.example.yemeksepeti_mobil_android_teame_hw3.data.entity.user.UserResponse
 import com.example.yemeksepeti_mobil_android_teame_hw3.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -15,12 +13,16 @@ import javax.inject.Inject
 class ProfileViewModel @Inject constructor(
     var apiRepository: TravelApiRepository
 ) : ViewModel() {
-    val userData: MutableLiveData<Resource<UserResponse>> = MutableLiveData()
 
     fun getUserData() : LiveData<Resource<UserResponse>> {
-        userData.postValue(Resource.loading())
-        val request = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMTI5NzM0YjNhYzU0MDAxNTY4MTc4YyIsIm5hbWUiOiJDdW5leXQgRXl1Ym9nbHUiLCJpYXQiOjE2Mjg2NzI0MDgsImV4cCI6MTYyODY5MDQwOH0.9QrnovN5NLLvMqIoQ5wuWQo-HLvGFkCJGSNTYg1cqdI"
+        val request = getToken()
         return (apiRepository.getUserData(request))
+    }
+    fun getToken() : String{
+        apiRepository.checkToken()?.let {
+            return(it)
+        }
+        return("no token provided")
     }
 
 }
