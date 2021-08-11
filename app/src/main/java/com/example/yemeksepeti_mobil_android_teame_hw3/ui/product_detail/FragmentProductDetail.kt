f423d5b7de4ffc7885900f0edb2f2efae96f59db
Cüneyt Atahan Eyüboğlu <ca.eyuboglu@gmail.com>
2021-08-11 15:47:43 +0300

    Product/Detail Loading Bar

--- app/src/main/java/com/example/yemeksepeti_mobil_android_teame_hw3/ui/product_detail/FragmentProductDetail.kt
+++ app/src/main/java/com/example/yemeksepeti_mobil_android_teame_hw3/ui/product_detail/FragmentProductDetail.kt
@@ -1,14 +1,17 @@
 package com.example.yemeksepeti_mobil_android_teame_hw3.ui.product_detail
 
 import android.annotation.SuppressLint
+import android.opengl.Visibility
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.bumptech.glide.Glide
+import com.example.yemeksepeti_mobil_android_teame_hw3.R
 import com.example.yemeksepeti_mobil_android_teame_hw3.databinding.FragmentProductDetailBinding
 import com.example.yemeksepeti_mobil_android_teame_hw3.util.Resource
 import dagger.hilt.android.AndroidEntryPoint
@@ -34,6 +37,13 @@ class FragmentProductDetail: Fragment() {
         if (hotelId != null) {
             viewModel.getHotelDatabyId(hotelId).observe(viewLifecycleOwner, {
                 when(it.status){
+                    Resource.Status.LOADING -> {
+                        _binding.productDetailHotelName.text = " "
+                        _binding.productDetailHotelDescription.text = " "
+                        _binding.productDetailHotelPrice.text = " "
+                        _binding.productDetailHotelPoint.text = " "
+                        _binding.productDetailHotelLocation.text = " "
+                    }
                     Resource.Status.SUCCESS -> {
                         val hotelData = it.data!!.hotelByIdData
                         val hotelStar = hotelData.star + ".0"
@@ -41,7 +51,10 @@ class FragmentProductDetail: Fragment() {
                         _binding.productDetailHotelDescription.text = hotelData.description
                         _binding.productDetailHotelPrice.text = "$${hotelData.price} "
                         _binding.productDetailHotelPoint.text = hotelStar
+                        _binding.productDetailHotelLocation.text = hotelData.location.substringAfterLast(",")
+                        _binding.productDetailLoadingBar.visibility = View.GONE
                         Glide.with(_binding.root.context).load(hotelData.imageUrl).into(_binding.productDetailBackground)
+                        _binding.productDetailBackground.scaleType = ImageView.ScaleType.CENTER_CROP
                     }
                 }
             })
